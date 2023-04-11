0f7b26058ae58b676ee1223adf69c764922de503
jannorth <116915245+jannorth@users.noreply.github.com>
2023-04-11 14:44:00 -0400

    updated PORT

--- server.js
+++ server.js
@@ -5,18 +5,19 @@ const db = require("./db/db.json");
 
 
 const app = express();
+
+const PORT = process.env.PORT || 3001;
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-const PORT = 3001;
-
-app.use(express.static("docs"));
+app.use(express.static("/public"));
 
 app.get("/", (req, res) =>
-  res.sendFile(path.join(__dirname, "docs/index.html"))
+  res.sendFile(path.join(__dirname, "public/index.html"))
 );
 
 app.get("/notes", (req, res) =>
-  res.sendFile(path.join(__dirname, "docs/notes.html"))
+  res.sendFile(path.join(__dirname, "public/notes.html"))
 );
 
 app.get("/api/notes", (req, res) => res.json(db));
